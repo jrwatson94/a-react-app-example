d07aad6297b22ce50f3c40a825d6f0d94e01a571
jrwatson94 <j.watsonreid@gmail.com>
2020-11-16 15:38:43 -0500

    Done

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
+
 // no need to modify anything in here. The second test is looking for the returned JSX from this component
-class ExampleComponent extends Component {
+class ExampleComponent extends React.Component {
   render() {
     return (
       <div className="example">
